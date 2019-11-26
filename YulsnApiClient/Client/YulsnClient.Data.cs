97c20efcff90f070b66df9ca5a041e9a0b3b84d3
Michael Fjeldsted <mf@juhlsen.com>
2019-11-26 22:51:04 +0100

    GetObjectByView

---
+++ YulsnApiClient/Client/YulsnClient.Data.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using YulsnApiClient.Models;
+using System.Net.Http;
+
+namespace YulsnApiClient.Client
+{
+    partial class YulsnClient
+    {
+        public Task<List<T>> GetObjectByView<T>(string viewName) =>
+            SendAsync<List<T>>($"api/v1/Data/View/{viewName}");
+    }
+}
