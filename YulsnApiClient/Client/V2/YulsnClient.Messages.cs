65e033f5e89116813125078ea0689477a480f843
Michael Fjeldsted <mf@yulsn.com>
2025-01-23 13:42:31 +0100

    GetActiveTriggerIdsAsync

---
+++ YulsnApiClient/Client/V2/YulsnClient.Messages.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using YulsnApiClient.Models.V2;
+
+namespace YulsnApiClient.Client
+{
+    partial class YulsnClient
+    {
+        /// <summary>
+        /// Sends a push message to a single contact
+        /// </summary>
+        public Task<string[]> GetActiveTriggerIdsAsync(YulsnMessageForm form) =>
+            SendAsync<string[]>(HttpMethod.Get, $"api/v2/{AccountId}/Messages/{form}/TriggerIds/Active", nameof(SendSinglePushMessageAsync), YulsnApiVersion.V2);
+    }
+}
