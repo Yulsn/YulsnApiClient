22f3978576743d371cff894cafb4a8d1309a2d24
CT-Yulsn <cta@yulsn.com>
2025-02-11 17:36:54 +0100

    feat(client): added GetSegmentContactStatusAsync

---
+++ YulsnApiClient/Client/V2/YulsnClient.Segments.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using YulsnApiClient.Models.V2;
+
+namespace YulsnApiClient.Client
+{
+    partial class YulsnClient
+    {
+        /// <summary>
+        /// Will get contact status for each segment.
+        /// </summary>
+        /// <param name="contactId">Id of the contact</param>
+        /// <param name="commaSepSegmentIds">Comma separated list of segment ids</param>
+        /// <param name="timeoutSec">Timeout in seconds</param>
+        /// <returns></returns>
+        public Task<YulsnSegmentContactStatus[]> GetSegmentContactStatusAsync(int contactId, string commaSepSegmentIds, int? timeoutSec = null) =>
+            SendAsync<YulsnSegmentContactStatus[]>(
+                HttpMethod.Get,
+                $"api/v2/{AccountId}/Segments/{contactId}/{commaSepSegmentIds}?{nameof(timeoutSec)}={timeoutSec}",
+                nameof(GetSegmentContactStatusAsync),
+                YulsnApiVersion.V2);
+    }
+}
---
+++ YulsnApiClient/Models/V2/YulsnSegment.cs
@@ -0,0 +1,8 @@
+﻿namespace YulsnApiClient.Models.V2
+{
+    public class YulsnSegmentContactStatus
+    {
+        public int SegmentId { get; set; }
+        public bool Contains { get; set; }
+    }
+}
