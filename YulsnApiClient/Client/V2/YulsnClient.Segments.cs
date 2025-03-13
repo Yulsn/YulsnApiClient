a8d220af2d6a0d3b734a91baa76921c95ac0cb9e
Michael Fjeldsted <mf@yulsn.com>
2025-03-13 13:01:01 +0100

    GetSegmentContactStatusAsync

--- YulsnApiClient/Client/V2/YulsnClient.Segments.cs
+++ YulsnApiClient/Client/V2/YulsnClient.Segments.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using YulsnApiClient.Models.V2;
@@ -17,8 +18,21 @@ namespace YulsnApiClient.Client
         public Task<YulsnSegmentContactStatus[]> GetSegmentContactStatusAsync(int contactId, IEnumerable<int> segmentIds, int? timeoutSec = null) =>
             SendAsync<YulsnSegmentContactStatus[]>(
                 HttpMethod.Get,
-                $"api/v2/{AccountId}/Segments/GetContactStatus/{contactId}/{string.Join(",", segmentIds)}?{nameof(timeoutSec)}={timeoutSec}",
+                $"api/v2/{AccountId}/Segments/GetContactStatus/{contactId}/{string.Join(",", segmentIds)}{(timeoutSec != null ? $"?{nameof(timeoutSec)}={timeoutSec}" : null)}",
                 nameof(GetSegmentContactStatusAsync),
                 YulsnApiVersion.V2);
+
+        /// <summary>
+        /// Will get contact status for a segment.
+        /// </summary>
+        /// <param name="contactId">Id of the contact</param>
+        /// <param name="segmentId">Id of the segment</param>
+        /// <param name="timeoutSec">Timeout in seconds. 30 is max</param>
+        /// <returns></returns>
+        public async Task<YulsnSegmentContactStatus> GetSegmentContactStatusAsync(int contactId, int segmentId, int? timeoutSec = null)
+        {
+            return (await GetSegmentContactStatusAsync(contactId, new int[] { segmentId }, timeoutSec).ConfigureAwait(false)).FirstOrDefault();
+        }
+
     }
 }
