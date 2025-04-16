d8a03e892ec9e89888b55da42214a848c21f6f60
Michael Fjeldsted <mf@yulsn.com>
2025-04-16 12:00:23 +0200

    SendSingleEmail2DispatchAsync

--- YulsnApiClient/Client/V1/YulsnClient.EmailDispatches.cs
+++ YulsnApiClient/Client/V1/YulsnClient.EmailDispatches.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using YulsnApiClient.Models.V1;
@@ -17,6 +19,38 @@ namespace YulsnApiClient.Client
             SendAsync<string>(new HttpRequestMessage(HttpMethod.Post, "api/v1/Email2Dispatches")
             {
                 Content = JsonContent(yulsnCreateEmail2DispatchDto)
+            });
+
+        public Task<string> SendSingleEmail2DispatchAsync(string triggerId, string email, params (string key, object value)[] dynamic) =>
+            CreateEmail2DispatchAsync(new YulsnCreateEmail2DispatchDto
+            {
+                TriggerId = triggerId,
+                Scope = new YulsnEmail2ScopeDto
+                {
+                    Emails = new Dictionary<string, YulsnEmail2ContactSettingsDto>
+                    {
+                        [email] = new YulsnEmail2ContactSettingsDto
+                        {
+                            Dynamic = dynamic.ToDictionary(kvp => kvp.key, kvp => kvp.value)
+                        }
+                    }
+                }
+            });
+
+        public Task<string> SendSingleEmail2DispatchAsync(string triggerId, int contactId, params (string key, object value)[] dynamic) =>
+            CreateEmail2DispatchAsync(new YulsnCreateEmail2DispatchDto
+            {
+                TriggerId = triggerId,
+                Scope = new YulsnEmail2ScopeDto
+                {
+                    Contacts = new Dictionary<int, YulsnEmail2ContactSettingsDto>
+                    {
+                        [contactId] = new YulsnEmail2ContactSettingsDto
+                        {
+                            Dynamic = dynamic.ToDictionary(kvp => kvp.key, kvp => kvp.value)
+                        }
+                    }
+                }
             });
     }
 }
