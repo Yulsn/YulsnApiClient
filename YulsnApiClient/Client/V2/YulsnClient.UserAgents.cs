3fde38c99409aacd25616e6637b70811891af9b3
Csaba <cta@yulsn.com>
2025-04-24 14:29:17 +0200

    feat(client): get user agents by id

---
+++ YulsnApiClient/Client/V2/YulsnClient.UserAgents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using YulsnApiClient.Models.V2;
+
+namespace YulsnApiClient.Client
+{
+    partial class YulsnClient
+    {
+        public Task<List<YulsnUserAgent>> GetUserAgentsAsync(List<int> ids) =>
+             SendAsync<List<YulsnUserAgent>>(HttpMethod.Post, $"api/v2/{AccountId}/UserAgents", ids, YulsnApiVersion.V2);
+    }
+}
---
+++ YulsnApiClient/Models/V2/YulsnUserAgent.cs
@@ -0,0 +1,8 @@
+﻿namespace YulsnApiClient.Models.V2
+{
+    public class YulsnUserAgent
+    {
+        public int Id { get; set; }
+        public string RawString { get; set; }
+    }
+}
