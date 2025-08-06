bc12102dd686ab6e5a4433c1fc25d956707d292a
Csaba <cta@yulsn.com>
2025-08-06 11:46:03 +0200

    feat(client): v2 point endpoints

---
+++ YulsnApiClient/Client/V2/YulsnClient.Points.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using YulsnApiClient.Models.V2;
+
+namespace YulsnApiClient.Client
+{
+    partial class YulsnClient
+    {
+        public Task<YulsnPoint> GetPointByIdAsync(int id) =>
+            SendAsync<YulsnPoint>(HttpMethod.Get, $"api/v2/{AccountId}/Points/{id}", YulsnApiVersion.V2);
+
+        public Task<List<YulsnPoint>> GetPointsPageAsync(int lastId, string type = null, int? take = null)
+        {
+            string url = $"api/v2/{AccountId}/Points/Page/{lastId}";
+
+            if (type != null)
+                url += $"?type={type}";
+
+            if (take.HasValue)
+                url += (type != null ? "&" : "?") + $"take={take.Value}";
+
+            return SendAsync<List<YulsnPoint>>(HttpMethod.Get, url, YulsnApiVersion.V2);
+        }
+
+        public Task<List<YulsnPoint>> GetPointsAsync(
+            int? contactId,
+            string sourceId,
+            string type,
+            DateTimeOffset? datetimeFrom,
+            DateTimeOffset? datetimeTo,
+            int? contactCompanyId,
+            int? storeId)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            if (contactId.HasValue)
+                parameters["contactId"] = contactId.Value.ToString();
+
+            if (!string.IsNullOrEmpty(sourceId))
+                parameters["sourceId"] = sourceId;
+
+            if (!string.IsNullOrEmpty(type))
+                parameters["type"] = type;
+
+            if (datetimeFrom.HasValue)
+                parameters["datetimeFrom"] = HttpUtility.UrlEncode(((DateTimeOffset)datetimeFrom).ToString("O"));
+
+            if (datetimeTo.HasValue)
+                parameters["datetimeTo"] = HttpUtility.UrlEncode(((DateTimeOffset)datetimeTo).ToString("O"));
+
+            if (contactCompanyId.HasValue)
+                parameters["contactCompanyId"] = contactCompanyId.Value.ToString();
+
+            if (storeId.HasValue)
+                parameters["storeId"] = storeId.Value.ToString();
+
+            string url = null;
+
+            foreach (var parameter in parameters)
+            {
+                if (url is null)
+                    url = $"api/v2/{AccountId}/Points?{parameter.Key}={parameter.Value}";
+                else
+                    url += $"&{parameter.Key}={parameter.Value}";
+            }
+
+            if (url is null)
+                throw new ArgumentException("At least one parameter must be provided.");
+
+            return SendAsync<List<YulsnPoint>>(HttpMethod.Get, url, YulsnApiVersion.V2);
+        }
+    }
+}
---
+++ YulsnApiClient/Models/V2/YulsnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YulsnApiClient.Models.V2
+{
+    public class YulsnPoint
+    {
+        public int Id { get; set; }
+        public int? ContactId { get; set; }
+        public int? ContactCompanyId { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string SourceId { get; set; }
+        public string SourceDescriptionId { get; set; }
+        public int? StoreId { get; set; }
+
+        public decimal Sum { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public decimal? BookValue { get; set; }
+        public decimal? PointCalculationBase { get; set; }
+        public int? ConverterTransactionId { get; set; }
+        public int? CancellerPointId { get; set; }
+        public bool IsCanceller { get; set; }
+    }
+}
