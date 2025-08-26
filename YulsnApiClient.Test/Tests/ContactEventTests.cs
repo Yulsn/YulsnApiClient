e0bce46f302cf4d6565275dbc66ca5b2d9ff1a18
Csaba <cta@yulsn.com>
2025-08-26 15:20:40 +0200

    test(contactevent): create and get tests

---
+++ YulsnApiClient.Test/Tests/ContactEventTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using YulsnApiClient.Client;
+using YulsnApiClient.Models.V1;
+using YulsnApiClient.Test.Abstractions;
+
+namespace YulsnApiClient.Test.Tests
+{
+    public class ContactEventTests(Setup setup) : IClassFixture<Setup>
+    {
+        private readonly YulsnClient yulsnClient = setup.ServiceProvider.GetService<YulsnClient>();
+        private readonly TestModel _model = setup.Repository.Model;
+
+        [Fact]
+        public async Task CreateContactEvent()
+        {
+            YulsnCreateContactEvent yulsnEvent = new()
+            {
+                Type = "test",
+                SubType = "sub_test",
+                ParentId = "integration_test",
+                ParentType = "xunit",
+                UniqueExtId = Guid.NewGuid().ToString(),
+                ContactId = _model.ValidContactId
+            };
+
+            {
+                await yulsnClient.CreateContactEventAsync(
+                    yulsnCreateContactEvents: [yulsnEvent],
+                    ignoreUnknownContacts: true);
+            }
+
+            {
+                List<YulsnReadContactEvent> list = await yulsnClient.GetContactEventsAsync<YulsnReadContactEvent>(type: yulsnEvent.Type, lastId: 0);
+
+                Assert.NotNull(list);
+                Assert.NotEmpty(list);
+            }
+
+            {
+                List<YulsnReadContactEvent> list = await yulsnClient.GetContactEventsAsync<YulsnReadContactEvent>(
+                    type: yulsnEvent.Type,
+                    subtype: yulsnEvent.SubType,
+                    from: DateTimeOffset.UtcNow.AddDays(-1),
+                    to: DateTimeOffset.UtcNow.AddHours(1),
+                    contactId: yulsnEvent.ContactId);
+
+                Assert.NotNull(list);
+                Assert.NotEmpty(list);
+                Assert.Contains(list, o => o.UniqueExtId == yulsnEvent.UniqueExtId);
+            }
+        }
+    }
+}
