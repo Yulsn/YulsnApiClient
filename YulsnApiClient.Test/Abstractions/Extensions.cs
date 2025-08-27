fa0f80ad31c5aef3069fe38645480b95c2201a11
Csaba <cta@yulsn.com>
2025-08-27 19:45:56 +0200

    test(contact): get with dynamic field test

---
+++ YulsnApiClient.Test/Abstractions/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace YulsnApiClient.Test.Abstractions
+{
+    public static class Extensions
+    {
+        public static string Surround(this string source, string start = null, string end = null, bool @if = true)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                return null;
+
+            if (@if)
+                return $"{start ?? string.Empty}{source}{end ?? start ?? string.Empty}";
+
+            return source;
+        }
+
+        public static bool TryGetValueAsString(this IDictionary<string, object> source, string key, out string value)
+        {
+            if (source.TryGetValue(key, out object v1) && v1 is string v2)
+            {
+                value = v2;
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+
+        public static bool TryGetValueAsLong(this IDictionary<string, object> source, string key, out long? value)
+        {
+            if (source.TryGetValue(key, out object v1) && v1 is long v2)
+            {
+                value = v2;
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+    }
+}
--- YulsnApiClient.Test/Tests/ContactTests.cs
+++ YulsnApiClient.Test/Tests/ContactTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -14,6 +15,11 @@ namespace YulsnApiClient.Test.Tests
         private class YulsnTestContact : YulsnContact
         {
             public string Email { get; set; }
+            public string Secret { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Phone { get; set; }
+            public byte[] Version { get; set; }
         }
 
         private readonly YulsnClient _yulsnClient = setup.ServiceProvider.GetService<YulsnClient>();
@@ -97,5 +103,123 @@ namespace YulsnApiClient.Test.Tests
                     throw;
             }
         }
+
+        [Fact]
+        public async Task Get_Ok_WhenFoundById()
+        {
+            int id = _model.ValidContactId;
+
+            Dictionary<string, object> response = await _yulsnClient.GetContactAsDictionaryByIdAsync<object>(id);
+
+            ValidateContactFields(response, expectedId: id, expectedDynamicField: _model.ValidContactDynamicStringFieldValue);
+        }
+
+
+        private (long id, string secret, string version, IDictionary<string, object> fields) ValidateContactFields(
+          Dictionary<string, object> fields,
+          int? expectedId = null,
+          string expectedSecret = null,
+          string expectedEmail = null,
+          object expectedPhone = null,
+          object expectedFirstName = null,
+          object expectedLastName = null,
+          object expectedDynamicField = null)
+        {
+            Assert.True(fields.TryGetValueAsLong(nameof(YulsnTestContact.Id), out long? _id));
+            Assert.NotNull(_id);
+
+            Assert.True(fields.TryGetValueAsString(nameof(YulsnTestContact.Secret), out string _secret));
+            Assert.NotNull(_secret);
+
+            Assert.True(fields.TryGetValueAsString(nameof(YulsnTestContact.Version), out string _version));
+            Assert.NotNull(_version);
+
+            bool hasEmail = fields.TryGetValueAsString(nameof(YulsnTestContact.Email), out string _email);
+            bool hasPhone = fields.TryGetValueAsString(nameof(YulsnTestContact.Phone), out string _phone);
+            bool hasFirstName = fields.TryGetValueAsString(nameof(YulsnTestContact.FirstName), out string _firstName);
+            bool hasLastName = fields.TryGetValueAsString(nameof(YulsnTestContact.LastName), out string _lastName);
+
+            Assert.Contains(_model.ValidContactDynamicStringFieldName, fields.Keys);
+            bool hasDynamicField = fields.TryGetValueAsString(_model.ValidContactDynamicStringFieldName, out string _dynamicField);
+
+            if (expectedId is not null)
+            {
+                Assert.Equal(expectedId, _id);
+            }
+
+            if (expectedSecret is not null)
+            {
+                Assert.Equal(expectedSecret, _secret);
+            }
+
+            if (expectedEmail is not null)
+            {
+                Assert.True(hasEmail);
+                Assert.NotNull(_email);
+                Assert.Equal(expectedEmail, _email);
+            }
+
+            if (expectedPhone is not null)
+            {
+                Assert.True(hasPhone);
+                Assert.NotNull(_phone);
+                Assert.Equal(expectedPhone, _phone);
+            }
+
+            if (expectedFirstName is not null)
+            {
+                Assert.True(hasFirstName);
+                Assert.NotNull(_firstName);
+                Assert.Equal(expectedFirstName, _firstName);
+            }
+
+            if (expectedLastName is not null)
+            {
+                Assert.True(hasLastName);
+                Assert.NotNull(_lastName);
+                Assert.Equal(expectedLastName, _lastName);
+            }
+
+            if (expectedDynamicField is not null)
+            {
+                Assert.True(hasDynamicField);
+                Assert.NotNull(_dynamicField);
+                Assert.Equal(expectedDynamicField, _dynamicField);
+            }
+
+            return (_id.Value, _secret, _version, fields);
+        }
+
+        /*
+GET api/v1/Contacts	
+Returns all contactIds in system
+
+GET api/v1/Contacts?field={field}&value={value}	
+Will return array of contacts by dynamic field or empty array if no contacts are found. The returned list of fields are cached.
+
+GET api/v1/Contacts/PatchFields	
+Will return a list of contact fields which can be PATCHed on a batch of contacts.
+
+GET api/v1/Contacts/DynamicFields	
+Will get Account specific fields. The fields will be valid to use in all requests, but will not be shown in the documentation, because it is account wide documentation.
+
+GET api/v1/Contacts/GetContactsBaseInfo	
+Returns basic info about all contacts in the system.
+
+GET api/v1/Contacts/Registration?timespan={timespan}&source={source}&ip={ip}	
+Will return array of registered contacts by Registration timespan, RegistrationSource or RegistrationIp (can combine). Will return empty array if nothing is found.
+
+GET api/v1/Contacts/{id}/PersonDataFile	
+Will return url of person data file
+
+POST api/v1/Contacts/{id}/Login	
+Validates a password for a specific user. Will return contact or HTTP 404 if contact is not found. The returned list of fields are cached.
+
+POST api/v1/Contacts/{id}/SetPassword	
+Sets the password for a user. Will return HTTP 200 or HTTP 404 if contact is not found.
+
+POST api/v1/Contacts/{id}/UnblockEmail	
+Unblocks a contact that is bounced by email. Returns false if the contact is not bounced by email.
+ */
     }
 }
