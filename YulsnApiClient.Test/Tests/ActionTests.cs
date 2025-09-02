f6f4870908d8f7d65a52570ba38b2223746d3277
Csaba <cta@yulsn.com>
2025-09-02 13:17:53 +0200

    test(action): get actions

---
+++ YulsnApiClient.Test/Tests/ActionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using YulsnApiClient.Client;
+using YulsnApiClient.Models.V1;
+using YulsnApiClient.Test.Abstractions;
+
+namespace YulsnApiClient.Test.Tests
+{
+    public class ActionTests(Setup setup) : IClassFixture<Setup>
+    {
+        private readonly YulsnClient yulsnClient = setup.ServiceProvider.GetService<YulsnClient>();
+
+        [Fact]
+        public async Task GetActions()
+        {
+            List<YulsnReadAction> response = await yulsnClient.GetActionsAsync();
+
+            Assert.NotNull(response);
+            Assert.True(response.Count > 0);
+        }
+    }
+}
---
+++ YulsnApiClient/Models/V1/YulsnAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YulsnApiClient.Models.V1
+{
+    public class YulsnReadAction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset LastModified { get; set; }
+    }
+}
